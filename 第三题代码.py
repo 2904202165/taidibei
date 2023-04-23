8bd002f47ed4c9ddd0aae403095385c692bf0bc1
2904202165 <85621473+2904202165@users.noreply.github.com>
2023-04-23 12:35:22 +0800

    Update 第三题代码.py

--- 第三题代码.py
+++ 第三题代码.py
@@ -1,26 +1,7 @@
-import pandas as pd
 import numpy as np
-from numpy.linalg import norm
+import pandas as pd
 
-
-# 构建技能要求向量和权重向量
-skills = ['C++', 'Java', 'Python', 'Machine Learning', 'Data Mining', 'Communication']
-weights = [0.2, 0.2, 0.2, 0.15, 0.15, 0.1]
-skill_req = np.array([0.9, 0.8, 0.7, 0.6, 0.5, 0.4])  # 技能要求向量
-
-def calc_similarity(s, r):
-    """
-    计算求职者技能掌握情况和岗位技能要求之间的匹配度
-    :param s: list, 求职者技能掌握情况向量
-    :param r: list, 岗位技能要求向量
-    :return: float, 匹配度
-    """
-    s_vec = np.array(s)
-    r_vec = np.array(r)
-    sim = np.dot(s_vec, r_vec) / (norm(s_vec) * norm(r_vec))
-    return sim
-
-# 读取求职者和招聘信息数据
+# 读取数据文件
 try:
     candidates = pd.read_csv('C:/Users/29042/PycharmProjects/pythonProject/result1-1.csv.csv')
     recruits = pd.read_csv('C:/Users/29042/PycharmProjects/pythonProject/result1-2.csv.csv')
@@ -30,41 +11,58 @@ except FileNotFoundError:
 except pd.errors.EmptyDataError:
     print("数据文件中没有数据，请检查数据文件是否为空。")
     exit(-1)
-# 计算每个求职者和每条招聘信息的匹配度和满意度，并记录非0匹配度的求职者和非0满意度的招聘信息
-matched_candidates = {}
-satisfied_recruits = {}
-for _, recruit_info in recruits.iterrows():
-    recruited_skills = recruit_info[skills].values.astype(float)
-    # 计算匹配度
-    recruit_similarities = {}
-    for _, candidate_info in candidates.iterrows():
-        candidate_skills = candidate_info[skills].values.astype(float)
-        sim = calc_similarity(candidate_skills, recruited_skills)
-        if sim > 0:
-            recruit_similarities[candidate_info['ID']] = sim
-    if len(recruit_similarities) > 0:
-        matched_candidates[recruit_info['ID']] = recruit_similarities
-    # 计算满意度
-    recruit_scores = {}
-    for _, candidate_info in candidates.iterrows():
-        candidate_skills = candidate_info[skills].values.astype(float)
-        score = weights.dot(candidate_skills * recruited_skills)
-        if score > 0:
-            recruit_scores[candidate_info['ID']] = score
-    if len(recruit_scores) > 0:
-        satisfied_recruits[recruit_info['ID']] = recruit_scores
 
-# 输出非0匹配度的求职者和非0满意度的招聘信息
-matched_df = pd.DataFrame.from_dict(matched_candidates, orient='index')
-matched_df.index.name = 'Recruit ID'
-long_matched_df = pd.melt(matched_df.reset_index(), id_vars=['Recruit ID'], var_name='Candidate ID',
-                           value_name='Matching Degree')
-matched_df = long_matched_df[long_matched_df['Matching Degree'] > 0].sort_values(by=['Recruit ID', 'Matching Degree'], ascending=[True, False])
-matched_df.to_csv('result3-1.csv', index=False)
+# 将 '不限'、'经验不限' 和时间范围字符串替换为 nan
+candidates.replace(['不限', '经验不限', '1-3年', '3-5年', '5-10年','5-7年','7年以上','互联网','计算机软件'], np.nan, inplace=True)
+recruits.replace(['不限', '经验不限', '1-3年', '3-5年', '5-10年','5-7年','7年以上','互联网','计算机软件'], np.nan, inplace=True)
 
-satisfied_df = pd.DataFrame.from_dict(satisfied_recruits, orient='index')
-satisfied_df.index.name = 'Candidate ID'
-long_satisfied_df = pd.melt(satisfied_df.reset_index(), id_vars=['Candidate ID'], var_name='Recruit ID',
-                           value_name='Satisfaction Score')
-satisfied_df = long_satisfied_df[long_satisfied_df['Satisfaction Score'] > 0].sort_values(by=['Candidate ID', 'Satisfaction Score'], ascending=[True, False])
-satisfied_df.to_csv('result3-2.csv', index=False)
+# 构建候选人能力矩阵和招聘人员能力矩阵，并计算匹配度和满意度
+skills = ['经验要求', '学历要求', '最低薪资', '最高薪资', '招聘人数', '营业范畴']
+candidates_skills = candidates[skills].values.astype(float)
+candidates_min_skills = candidates['Minimum'].values.astype(float)
+recruited_skills = []
+for idx, recruit_info in recruits.iterrows():
+    recruited_skills.append(recruit_info[skills].values.astype(float))
+recruited_skills = np.array(recruited_skills)
+
+matching_matrix = recruited_skills @ candidates_skills.T
+
+matching_degree = []
+for i in range(len(matching_matrix)):
+    if np.any(np.isnan(matching_matrix[i])) or np.any(matching_matrix[i] < recruits.iloc[i].Requirements):
+        matching_degree.append(0)
+    else:
+        matching_degree.append(np.nanmean(matching_matrix[i] * recruits.iloc[i][skills]))
+matching_degree = np.array(matching_degree)
+
+satisfaction_degree = []
+for i in range(len(matching_matrix.T)):
+    max_matching_index = np.nanargmax(matching_matrix.T[i])
+    if np.any(np.isnan(matching_matrix.T[i])) or np.any(matching_matrix.T[i][max_matching_index] < candidates_min_skills[i]):
+        satisfaction_degree.append(0)
+    else:
+        satisfaction_degree.append(np.nanmean(matching_matrix.T[i] * candidates.iloc[i][skills]))
+satisfaction_degree = np.array(satisfaction_degree)
+
+# 选取满足条件的数据，分别输出为结果文件
+resul3_1 = pd.DataFrame()
+resul3_2 = pd.DataFrame()
+for idx, recruit_info in recruits.iterrows():
+    if matching_degree[idx] > 0:
+        resul3_1 = resul3_1.append(pd.Series({
+            "Name": recruit_info.Name,
+            "Matching Degree": matching_degree[idx]
+        }), ignore_index=True)
+for idx, candidate_info in candidates.iterrows():
+    if satisfaction_degree[idx] > 0:
+        resul3_2 = resul3_2.append(pd.Series({
+            "Name": candidate_info.Name,
+            "Satisfaction Degree": satisfaction_degree[idx]
+        }), ignore_index=True)
+
+resul3_1.sort_values(by="Matching Degree", ascending=False, inplace=True)
+resul3_2.sort_values(by="Satisfaction Degree", ascending=False, inplace=True)
+
+resul3_1.to_csv("result3-1.csv", index=False)
+resul3_2.to_csv("result3-2.csv", index=False)
+print("结果已经保存到 result3-1.csv 和 result3-2.csv 文件中。")
